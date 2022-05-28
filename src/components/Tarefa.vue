098221804fb3bc16398c11c2908b274e730657fd
Gustavo Araújo <gustavo.avk@msn.com>
2022-05-28 06:07:49 -0300

    feature(box component): stores task information and elapsed time

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,34 @@
+<template>
+    <Box>
+        <div class="columns">
+            <div class="column is-7">
+                {{ tarefa.descricao || 'Tarefa sem descrição' }}
+            </div>
+            <div class="column">
+                <Cromometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+            </div>
+        </div>
+    </Box>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import ITarefa from '../interfaces/Itarefa'
+import Cromometro from './Cronometro.vue'
+import Box from './Box.vue'
+
+export default defineComponent({
+    name: 'Terefa',
+    props: {
+        tarefa: {
+            type: Object as PropType<ITarefa>,
+            required: true
+        }
+    },
+    components: {
+        Cromometro,
+        Box
+    }
+})
+</script>
+
