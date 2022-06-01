d004e3cc1572eec3f133a8b91bfd4de71a112436
Gustavo Araújo <gustavo.avk@msn.com>
2022-06-01 00:40:03 -0300

    feature(vuex): new mutation to change projects

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,19 +1,31 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Tarefas from '../views/Tarefas.vue'
 import Projetos from '../views/Projetos.vue'
+import Formulario from "@/views/projetos/Formulario.vue";
 
 const rotas: RouteRecordRaw[] = [
     {
-        path: '/',
-        name: 'Tarefas',
-        component: Tarefas
+        path: "/",
+        name: "Tarefas",
+        component: Tarefas,
     },
     {
-        path: '/projetos',
-        name: 'Projetos',
-        component: Projetos
-    }
-]
+        path: "/projetos",
+        name: "Projetos",
+        component: Projetos,
+    },
+    {
+        path: "/projetos/novo",
+        name: "Novo projetos",
+        component: Formulario,
+    },
+    {
+        path: "/projetos/:id",
+        name: "Editar projeto",
+        component: Formulario,
+        props: true
+    },
+];
 
 const roteador = createRouter({
     history: createWebHashHistory(),
