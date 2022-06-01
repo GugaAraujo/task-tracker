b34a878ce90325d6829772bec0db4ddf73ca5a03
Gustavo Araújo <gustavo.avk@msn.com>
2022-06-01 01:56:26 -0300

    feature(vuex): Controlling the status of Tasks

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,15 @@
 import IProjeto from "@/interfaces/IProjeto";
+import ITarefa from "@/interfaces/Itarefa";
 import { InjectionKey } from "vue";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
-import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO } from "./tipo-mutacoes";
+import { ADICIONA_PROJETO, ADICIONA_TAREFA, ALTERA_PROJETO, ATUALIZA_TAREFA, EXCLUIR_PROJETO, REMOVE_TAREFA } from "./tipo-mutacoes";
 
 // No Vuex 4, além de exportamos o store, precisamos
 // exportar também uma chave de acesso
 
 interface Estado {
-    projetos: IProjeto[]
+    projetos: IProjeto[],
+    tarefas: ITarefa[]
 }
 
 export const key: InjectionKey<Store<Estado>> = Symbol()
@@ -16,6 +18,7 @@ export const key: InjectionKey<Store<Estado>> = Symbol()
 export const store = createStore<Estado>({
     state: {
         projetos: [],
+        tarefas: [],
     },
 
     mutations: {
@@ -33,7 +36,18 @@ export const store = createStore<Estado>({
             state.projetos[index] = projeto;
         },
         [EXCLUIR_PROJETO](state, id: string) {
-            state.projetos = state.projetos.filter(proj => proj.id != id)
+            state.projetos = state.projetos.filter((proj) => proj.id != id);
+        },
+        [ADICIONA_TAREFA](state, tarefa: ITarefa) {
+            tarefa.id = new Date().toISOString();
+            state.tarefas.push(tarefa);
+        },
+        [ATUALIZA_TAREFA](state, tarefa: ITarefa) {
+            const indice = state.tarefas.findIndex((p) => p.id == tarefa.id);
+            state.tarefas[indice] = tarefa;
+        },
+        [REMOVE_TAREFA](state, id: string) {
+            state.projetos = state.projetos.filter((p) => p.id != id);
         }
     },
 });
