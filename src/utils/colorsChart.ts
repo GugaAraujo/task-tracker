e5c922b64b9e58aef8cd65719cfe6fcdd7062132
Gustavo Araújo <gustavo.avk@msn.com>
2022-06-08 10:10:58 -0300

    feature(charts): New collor list

---
+++ src/utils/colorsChart.ts
@@ -0,0 +1,14 @@
+import * as am4core from "@amcharts/amcharts4/core";
+
+const colorsChart = [
+    am4core.color("#ffa600"),
+    am4core.color("#ff6361"),
+    am4core.color("#A2B969"),
+    am4core.color("#58508d"),
+    am4core.color("#bc5090"),
+    am4core.color("#4196BC"),
+    am4core.color("#AA4643"),
+    am4core.color("#F6CC34"),
+];
+
+export default colorsChart;
