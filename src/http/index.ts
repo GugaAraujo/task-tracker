b5e377829da9baf8606691778d6320591fc50f7f
Gustavo Araujo <gustavoaraujo.dev@gmail.com>
2022-10-23 00:35:02 -0300

    refactor(axios): update API baseURL

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 
 const clientHttp: AxiosInstance = axios.create({
-    baseURL: "https://task-tracker-api-moleculer.herokuapp.com",
+    baseURL: "https://task-tracker-api-moleculer.herokuapp.com/api",
 });
 
 export default clientHttp
