d4dec287fc4a82fdcbb67c13aefcba0228d2df28
Gustavo Araujo <gustavoaraujo.dev@gmail.com>
2022-10-28 08:50:01 -0300

    feature(user): username notifications

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,4 +1,8 @@
+import roteador from '@/roteador';
 import axios, { AxiosInstance } from 'axios'
+import { TipoNotificacao } from "@/interfaces/INotificacao";
+import { NOTIFICAR } from "@/store/tipo-mutacoes";
+import { store } from "@/store"
 
 const clientHttp: AxiosInstance = axios.create({
     baseURL: "https://task-tracker-api-moleculer.herokuapp.com/api",
@@ -16,5 +20,79 @@ clientHttp.interceptors.request.use(
         return Promise.reject(error);
     }
 );
+
+clientHttp.interceptors.response.use(null, error => {
+    const { status, message, name } = error.response.data;
+    console.log(error.response.data)
+
+    if(status === 401) {
+        roteador.push('/login');
+    } else if (status === 403) {
+        store.commit(NOTIFICAR, {
+            titulo: "Não permitido",
+            texto: 'Seu usuário não tem permissão para realizar esta operação',
+            tipo: TipoNotificacao.FALHA,
+        }, { root: true });
+        throw new Error(error);
+    }
+
+    if (message === 'This email already exists') {
+        store.commit(NOTIFICAR, {
+            titulo: "E-mail já existente",
+            texto: "Por favor, escolha outro e-mail para cadastrar",
+            tipo: TipoNotificacao.FALHA,
+        }, { root: true });
+        throw new Error(error);
+
+    } if (message === 'Passwords must be the same!') {
+        store.commit(NOTIFICAR, {
+            titulo: "Senhas diferentes",
+            texto: "As senhas devem ser iguais",
+            tipo: TipoNotificacao.FALHA,
+        }, { root: true });
+        throw new Error(error);
+
+    } else if (message === 'the database system is starting up') {
+        store.commit(NOTIFICAR, {
+            titulo: "Ooops...",
+            texto: "O bando de dados se encontra em manutenção",
+            tipo: TipoNotificacao.FALHA,
+        }, { root: true });
+        throw new Error(error);
+
+    } else if (message === 'Email is not found')  {
+        store.commit(NOTIFICAR, {
+            titulo: "Não autorizado",
+            texto: 'Confira seu e-mail',
+            tipo: TipoNotificacao.FALHA,
+        }, { root: true });
+        throw new Error(error);
+
+    } else if (message === 'Password incorrect') {
+        store.commit(NOTIFICAR, {
+            titulo: "Não autorizado",
+            texto: 'Confira sua senha',
+            tipo: TipoNotificacao.FALHA,
+        }, { root: true });
+        throw new Error(error);
+
+    } else if (message === 'Parameters validation error!') {
+        store.commit(NOTIFICAR, {
+            titulo: "Erro",
+            texto: 'Confira se os campos foram registrados corretamente',
+            tipo: TipoNotificacao.FALHA,
+        }, { root: true });
+        throw new Error(error);
+
+    } else {
+        store.commit(NOTIFICAR, {
+            titulo: "Erro",
+            texto: `${message}`,
+            tipo: TipoNotificacao.FALHA,
+        }, { root: true });
+        throw new Error(error);
+
+    }
+});
 
 export default clientHttp;
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,9 +3,9 @@
         <section class="column is-8 container">
             <form @submit.prevent="register" class="box">
                 <div class="field">
-                    <label for="register" class="label"> Nome </label>
+                    <label for="register" class="label"> Username </label>
                     <div class="control">
-                        <input type="name" class="input" v-model="name" id="name" />
+                        <input type="name" class="input" v-model="username" id="username" />
                     </div>
                 </div>
                 <div class="field">
@@ -35,9 +35,12 @@
 </template>
 <script setup lang="ts">
 import http from "@/http";
+import { useStore } from "@/store";
+import { REGISTER_USER } from "@/store/tipo-acoes";
 import { ref } from "vue";
+const store = useStore();
 
-const name = ref(null);
+const username = ref(null);
 const email = ref(null);
 const password = ref(null);
 const confirmPwd = ref(null);
@@ -45,19 +48,15 @@ const isLoading = ref(false);
 
 async function register() {
     isLoading.value = true;
-    return http.post("/users/create", {
-        name: name.value,
+    store.dispatch(REGISTER_USER, {
+        username: username.value,
         email: email.value,
         password: password.value,
         confirmPwd: confirmPwd.value,
         agreeTerms: true,
-    }).then(() => {
-        this.$router.push('Login')
-    }).catch((err) => {
-        console.log(err);
     }).finally(() => {
         isLoading.value = false;
-    });
+    })
 }
 
 </script>
