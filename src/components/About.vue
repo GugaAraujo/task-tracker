51709b5886a27de39b8a470c7a76494cad4eabd5
Gustavo Araujo <gustavoaraujo.dev@gmail.com>
2023-02-17 12:31:18 -0300

    feat: add about component

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="mt-6 mb-4">
+        <h2 class="is-size-4 mb-5 has-text-weight-semibold">
+            Bem vindo ao Task Tracker
+        </h2>
+        <p class="is-size-5">
+            <span class="has-text-weight-semibold">
+                Task Tracker
+            </span>
+            é uma plataforma para gerenciamento de atividades.
+        </p>
+        <p class="is-size-5">
+            Com ele é possível contabilizar o tempo de cada tarefa e vincular a um projeto.
+        </p>
+    </div>
+</template>
